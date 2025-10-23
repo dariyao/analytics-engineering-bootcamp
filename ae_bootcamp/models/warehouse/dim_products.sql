677fab8433c12f9f4b3d5cb94e95ced4f2712f6e
Dariya Ostrovska <dariyaostrovska@gmail.com>
2025-10-23 12:20:49 +0100

    Created Dimensional Data Warehouse Layer

---
+++ ae_bootcamp/models/warehouse/dim_products.sql
@@ -0,0 +1,31 @@
+WITH source AS (
+    SELECT
+        p.id AS product_id,
+        p.product_code,
+        p.product_name,
+        p.description,
+        s.company AS supplier_company,
+        p.standard_cost,
+        p.list_price,
+        p.reorder_level,
+        p.target_level,
+        p.quantity_per_unit,
+        p.discontinued,
+        p.minimum_reorder_quantity,
+        p.category,
+        p.attachments,
+        current_timestamp() AS insertion_timestamp,
+    FROM {{ ref('stg_products') }} p
+    LEFT JOIN {{ ref('stg_suppliers') }} s
+    ON s.id = p.supplier_id
+),
+unique_source AS (
+    SELECT *,
+        row_number() over(partition by product_id) AS row_number
+    FROM source
+)
+SELECT *
+EXCEPT
+    (row_number),
+FROM unique_source
+WHERE row_number = 1
